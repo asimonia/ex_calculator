e3c4b87978c189a34598f8b514bc4e648c801a1f
asimonia <alex.simonian@gmail.com>
2016-10-18 13:55:08 -0500

    Add js

--- calculator.js
+++ calculator.js
@@ -5,47 +5,36 @@ var add = document.getElementById("add");
 var subtract = document.getElementById("subtract");
 var multiply = document.getElementById("multiply");
 var divide = document.getElementById("divide");
-var equals = document.getElementById("equals");
-
-
-// Register event listeners with elements on page
-add.addEventListener("click", function() {alert("add");});
-subtract.addEventListener("click", function() {alert("subtract");});
-multiply.addEventListener("click", function() {alert("multiply");});
-divide.addEventListener("click", function() {alert("divide");});
-submit.addEventListener("click", function() {alert("submit");});
-
-
 
 var multiplier = function (x, y) {
-  return x * y;
-}
+	return x * y;
+};
 
 
 var adder = function (x, y) {
-  return x + y;
-}
+	return x + y;
+};
 
 
 var subtracter = function (x, y) {
-  return x - y;
-}
+	return x - y;
+};
 
 
 var divider = function (x, y) {
-  return x / y;
-}
+	return x / y;
+};
 
 
 
-/*
-  Create a function that accepts three arguments.
-    1. First number
-    2. Second number
-    3. A function that performs an operation on them
+// Register event listeners with elements on page
+add.addEventListener("click", function() { perform(x.value, y.value, adder); }, false);
+subtract.addEventListener("click", function() { perform(x.value, y.value, subtracter); }, false);
+multiply.addEventListener("click", function() { perform(x.value, y.value, multiplier); }, false);
+divide.addEventListener("click", function() { perform(x.value, y.value, divider); }, false);
 
-  Return the value of the operation.
- */
-function (x, y, operation) {
-  
+
+function perform(x, y, f) {
+	console.log(x.value + " " + y.value);
+	return f(x, y);
 }
